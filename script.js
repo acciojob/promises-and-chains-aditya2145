2f847d94dad2df93b8df2b485007d9d3224f85cf
aditya2145 <148987683+aditya2145@users.noreply.github.com>
2024-10-15 18:02:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const form = document.getElementById('ageForm');
+
+form.addEventListener('submit', () => {
+	event.preventDefault();
+	const name = document.getElementById('name').value;
+	const age = document.getElementById('age').value;
+	age = parseInt(age);
+
+	const agePromise = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if(age > 18) {
+				resolve();
+			}
+			else{
+				reject();
+			}
+		}, 4000)
+	});
+
+	agePromise.then(() => {
+		alert(`Welcome, ${name}. You can vote.`)
+	})
+	.catch(() => {
+		alert(`Oh sorry ${name}. You aren't old enough.`)
+	})
+})
